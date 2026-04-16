987359bf59c85036bf23c8741935488ce2b9d06e
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-04-16 16:07:11 -0300

    fix(layout): adiciona navbar global para restaurar navegação

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,17 +7,23 @@ import Orcamento from "./pages/Orcamento";
 import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
 
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/catalogo/:category" component={Catalogo} />
-      <Route path="/equipamentos/:slug" component={Produto} />
-      <Route path="/orcamento" component={Orcamento} />
+    <>
+      <Navbar />
 
-      <Route path="/admin-panel" component={AdminDashboard} />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/catalogo/:category" component={Catalogo} />
+        <Route path="/equipamentos/:slug" component={Produto} />
+        <Route path="/orcamento" component={Orcamento} />
 
-      <Route component={NotFound} />
-    </Switch>
+        <Route path="/admin-panel" component={AdminDashboard} />
+
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
