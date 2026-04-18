f83f56d7f137553b0a9fc43144eeadc1eaf49d03
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-04-18 16:43:21 -0300

    git add . git commit -m "feat: adiciona rota da página Producao preservando estrutura global" git push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,41 @@
 import { Route, Switch, useLocation } from "wouter";
+import { CartProvider } from "./contexts/CartContext";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Catalogo from "./pages/Catalogo";
 import Produto from "./pages/Produto";
 import Orcamento from "./pages/Orcamento";
+import Producao from "./pages/Producao";
 import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
 
-import Navbar from "./components/Navbar";
-import { CartProvider } from "./contexts/CartContext";
-
-export default function App() {
+function AppContent() {
   const [location] = useLocation();
-
   const isAdmin = location.startsWith("/admin-panel");
 
   return (
-    <CartProvider>
-      {/* ✅ NAVBAR SÓ NO SITE PÚBLICO */}
+    <div className="min-h-screen bg-background text-foreground">
       {!isAdmin && <Navbar />}
 
       <Switch>
-        {/* SITE */}
         <Route path="/" component={Home} />
+        <Route path="/catalogo" component={Catalogo} />
         <Route path="/catalogo/:category" component={Catalogo} />
         <Route path="/equipamentos/:slug" component={Produto} />
         <Route path="/orcamento" component={Orcamento} />
-
-        {/* ADMIN */}
+        <Route path="/producao" component={Producao} />
         <Route path="/admin-panel" component={AdminDashboard} />
-
         <Route component={NotFound} />
       </Switch>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <CartProvider>
+      <AppContent />
     </CartProvider>
   );
 }
