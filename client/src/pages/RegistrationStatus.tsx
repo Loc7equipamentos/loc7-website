1f91bb6d265fb8f5ed9446933c9b08f923e279a3
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-04-25 11:08:26 -0300

    fix(build): add missing RegistrationStatus page

---
+++ client/src/pages/RegistrationStatus.tsx
@@ -0,0 +1,7 @@
+export default function RegistrationStatus() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      Status do cadastro
+    </div>
+  );
+}
