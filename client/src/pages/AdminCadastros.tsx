143816e808e22e3f9f6cf79568b93f8b95ddb423
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-04-27 15:26:14 -0300

    fix(admin): ativa botão editar (click e navegação corrigidos)

--- client/src/pages/AdminCadastros.tsx
+++ client/src/pages/AdminCadastros.tsx
@@ -226,16 +226,15 @@ export default function AdminCadastros() {
                       </button>
                     </Link>
 
-                    <button
-                      type="button"
-                      onClick={() =>
-                        (window.location.href = `/admin-panel/cadastro/${c.id}`)
-                      }
-                      className="bg-black text-white text-xs px-4 py-2 rounded-md hover:bg-gray-800 transition"
-                    >
-                      Editar
-                    </button>
-                  </td>
+                   <button
+  type="button"
+  onClick={() => {
+    window.location.assign(`/admin-panel/cadastro/${c.id}`);
+  }}
+ className="bg-black text-white text-xs px-4 py-2 rounded-md hover:bg-gray-800 transition relative z-50 pointer-events-auto"
+>
+  Editar
+</button>                  </td>
                 </tr>
               ))}
 
