d58f07a84556b772a51faa023f0f01dc0b796c29
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-04 18:08:01 -0300

    fix: corrige sintaxe da largura da logo no header

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -146,8 +146,8 @@ export default function Navbar() {
             <img
               src="/loc7-logo-header.png"
               alt="Loc 7 Equipamentos"
-              className="absolute left-[-6px] md:left-[-12px] top-[62%] -translate-y-1/2 w-[200px] md:w-[350px] h-auto transition-transform duration-300 group-hover:scale-[1.03]"
-          </Link>
+             className="absolute left-[-6px] md:left-[-12px] top-[62%] -translate-y-1/2 w-[200px] md:w-[350px] h-auto transition-transform duration-300 group-hover:scale-[1.03]"
+              </Link>
 
           {/* Navegação desktop / mobile trigger */}
           <div className="flex flex-col flex-1 relative">
