8e134ae9660d8002e677854e7dcd0d1c07876e7f
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-05 16:27:41 -0300

    feat: ajusta exibição responsiva dos destaques

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -262,7 +262,7 @@ function HomeFeaturedCard({ product }: HomeFeaturedCardProps) {
   className="group block"
 >
       <div
-       className="h-full overflow-hidden rounded-xl border border-black/5 bg-white shadow-[0_16px_42px_rgba(0,0,0,0.11)] transition-all duration-300 ease-out hover:-translate-y-[2px] hover:shadow-[0_22px_54px_rgba(0,0,0,0.15)]"
+      className="h-full overflow-hidden rounded-xl border border-black/5 bg-white shadow-[0_16px_42px_rgba(0,0,0,0.11)] transition-all duration-300 ease-out hover:-translate-y-[2px] hover:shadow-[0_22px_54px_rgba(0,0,0,0.15)]"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
