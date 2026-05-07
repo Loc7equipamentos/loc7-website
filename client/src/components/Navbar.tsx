79be22c255c60eba45fac9a6b3f68327bb1614a2
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-07 12:51:43 -0300

    fix: corrige estrutura do wrapper do menu desktop

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -153,7 +153,8 @@ export default function Navbar() {
           {/* Navegação desktop / mobile trigger */}
           <div className="flex flex-col flex-1 relative">
             <div
- <div className="flex items-center justify-center h-20 md:h-[72px] flex-1 md:translate-y-[44px]">              <div className="hidden md:flex items-center gap-10 lg:gap-12 justify-center flex-1 relative overflow-visible">
+ <div className="flex items-center justify-center h-20 md:h-[72px] flex-1 md:translate-y-[44px]">       
+   <div className="hidden md:flex items-center gap-10 lg:gap-12 justify-center flex-1 relative overflow-visible">
                 {navLinks.map((link) => (
                   <div
                     key={link.name}
