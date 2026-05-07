a80fae388d83db7ab6d6559cde1229a678310b98
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-07 15:46:56 -0300

    refactor: centraliza itens do dropdown de equipamentos

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -191,7 +191,7 @@ export default function Navbar() {
                             <Link
                               key={cat.name}
                               href={cat.href}
-                              className="block px-4 py-2 text-xs font-medium tracking-wide text-white hover:bg-gray-900 transition"
+                             className="block px-4 py-2 text-center text-xs font-medium tracking-wide text-white/80 transition-all duration-200 hover:scale-[1.02] hover:bg-white/[0.04] hover:text-white"
                             >
                               {cat.name}
                             </Link>
