ad51bf4804fbba1328b901ef3b270714c95fab00
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-07 12:29:23 -0300

    refactor: refina eixo central do menu desktop

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -152,7 +152,7 @@ export default function Navbar() {
 
           {/* Navegação desktop / mobile trigger */}
           <div className="flex flex-col flex-1 relative">
-            <div className="flex items-center justify-center h-20 md:h-[72px] flex-1 md:translate-y-[20px]">
+            <div className="flex items-center justify-center h-20 md:h-[72px] flex-1 md:translate-y-[30px]">
               <div className="hidden md:flex items-center gap-10 lg:gap-12 justify-center flex-1 relative overflow-visible">
                 {navLinks.map((link) => (
                   <div
