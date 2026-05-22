56e59c0645c43b4ef6689ce1814313c17d17d51a
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-05-22 18:46:37 -0300

    style(navbar): aumenta altura inferior do header para preparar barra de categorias

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -281,7 +281,7 @@ export default function Navbar() {
         isScrolled ? "bg-black shadow-lg shadow-black/30" : "bg-black"
       }`}
     >
-      <div className="container md:min-h-[120px]">
+      <div className="container md:min-h-[165px]">
         <div className="flex items-stretch justify-between overflow-visible">
           <Link
             href="/"
