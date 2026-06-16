fa9a62adc2f8b11a82c07ca8df53377ef99e74a7
Loc7equipamentos <loc7@loc7equipamentos.com.br>
2026-06-16 15:09:43 -0300

    refine mobile header icon alignment

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -434,7 +434,7 @@ export default function Navbar() {
                 </Link>
               </div>
 
-              className="absolute right-[-6px] top-1/2 flex -translate-y-1/2 translate-y-[0px] items-center gap-3 md:hidden"
+              <div className="absolute right-[-6px] top-1/2 flex -translate-y-1/2 items-center gap-3 md:hidden">
                 <button
                   type="button"
                   onClick={() => {
